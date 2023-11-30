583cd5b0c5e1d9d66e55ee2fdd1926f37c79145e
farid <dekfarid2015@gmail.com>
2023-12-01 00:13:34 +0700

    praktikum php MuhammadFarid

---
+++ Chapter08/714230070_MuhammadFarid/strlen.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>fungsi : string</title>
+    </head>
+    <body>
+        <h2>fungsi : string strlen()</h2>
+        <?php
+        $string="Pemrograman web PHP";
+        $jml=strlen($string);
+        echo ("<i>\"$string\"</i> <br>");
+        echo ("Banyak Karakter Adalah $jml");
+        ?>
+    </body>
+</html>
---
+++ Chapter08/714230070_MuhammadFarid/ucfirst.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>fungsi : string</title>
+    </head>
+    <body>
+        <h2>fungsi : string ucfirst()</h2>
+        <?php
+        $string="Pemrograman web PHP";
+        $word=ucfirst($string);
+        echo ("<i>\"$string\"</i> <br>");
+        echo ("Banyak ucfirst Adalah <i>\"$word\"</i>");
+        ?>
+    </body>
+</html>
