8e5f72e2a3c037fee10d2564cb3bc8d558c3fb1e
FARAZIFTAN <mahasiswaulbiit@gmail.com>
2023-11-30 22:58:11 +0700

    Tugas_Rajif_PHP

---
+++ Chapter08/latihan/if.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<head>
+    
+    <title>if</title>
+</head>
+<body>
+    <h2>Pengguna kontrol if</h2>
+    <?php
+    $umur=18;
+    echo ("Umur anda $umur tahun <br>");
+    if ($umur>=17)
+    {
+        echo ("Anda boleh masuk");
+    }
+    ?>
+</body>
+</html>
