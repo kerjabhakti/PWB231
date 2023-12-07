cc4c4435dc9f2e6ac7d03d989736ee6c18c07316
Agung <agungihsannudin@gmail.com>
2023-12-07 20:59:53 +0700

    Tugas Modul 10 Agung

---
+++ Chapter09/714230036_Agung/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pendaftran Mahasiswa Baru ULBI</title>
+</head>
+<body>
+    <header>
+        <h3>Pendaftran Mahasiswa Baru</h3>
+        <h1>Universitas Logistik dan Bisnis Internasional</h1>
+</header>
+
+<h4>Menu</h4>
+<nav>
+    <ul>
+        <li><a href="form.data.php">Daftar</a></li>
+        <li><a href="list-maba.php">Pendaftar</a></li>
+</ul>
+</nav>
+    
+</body>
+</html>
