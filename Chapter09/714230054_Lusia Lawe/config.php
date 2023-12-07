8aa5ddce8372aac23864cb6856342220634e85ee
Lusia1301 <bareklusia1@gmail.com>
2023-12-07 21:49:23 +0700

    Praktikum PHP Lusia

---
+++ Chapter09/714230054_Lusia Lawe/config.php
@@ -0,0 +1,12 @@
+<?php
+$_server = "localhost";
+$user = "root";
+$password = "";
+$namadb = "mahasiswabaru";
+
+$db = mysqli_connect($_server, $user, $password, $namadb);
+
+if(!$db){
+    die("gagal terhubung dengan database : " . mysqli_connect_error());
+}
+?>
